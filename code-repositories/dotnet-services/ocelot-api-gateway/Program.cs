ee0535a52b7b041c102c20e81aa5635c60c5ed22
Buu HQ <huynhquocbuu@gmail.com>
2023-04-22 12:14:53 +0700

    22222

--- code-repositories/dotnet-services/ocelot-api-gateway/Program.cs
+++ code-repositories/dotnet-services/ocelot-api-gateway/Program.cs
@@ -3,7 +3,8 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json");
+builder.Configuration.AddJsonFile("ocelot.json",optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -11,7 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 
@@ -27,10 +28,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.UseOcelot().Wait();
-
 app.MapControllers();
 
-
+await app.UseOcelot();
 
 app.Run();
